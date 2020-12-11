89bc6ac2e046ec270d0d6ad679df423951f8006c
yuki384 <31533303+yuki384@users.noreply.github.com>
2020-12-11 17:54:50 +0900

    イラスト追加

--- main.js
+++ main.js
@@ -10,11 +10,22 @@ window.onload = (event) => {
   }
   el.innerHTML = greet;
 
-  document.getElementById('present-l').addEventListener('click', () => {
-    hideMessage();
-    let result = document.getElementById('p1')
-    result.classList.add('visible');
-  });
+  clickKazari('present-l', 'p1');
+  clickKazari('present-m', 'p2');
+  clickKazari('present-s', 'p3');
+  clickKazari('kazari-s', 'p4');
+  clickKazari('kazari-l', 'p5');
+  clickKazari('bell-s', 'p6');
+  clickKazari('bell-l', 'p7');
+
+  function clickKazari(kazariName, id) {
+    document.getElementById(kazariName).addEventListener('click', () => {
+      hideMessage();
+      let result = document.getElementById(id);
+      result.classList.add('visible');
+    });
+  }
+
   let close = document.querySelectorAll(".message-close-x");
   close.forEach((target) => {
     target.addEventListener('click', () => {
